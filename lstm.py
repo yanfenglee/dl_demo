eb4b3244042c04196f340e22c5d78d359c3cc823
liyanfeng <yanfeng.li@medishare.cn>
2017-09-14 15:17:00 +0800

    aa

---
+++ lstm.py
@@ -0,0 +1,120 @@
+import tensorflow as tf
+from tensorflow.python import debug as tf_debug
+
+class LSTMCell:
+    def __init__(self, input_size, num_units):
+        self.input_size = input_size
+        self.num_units = num_units
+
+        self.igate = self.init_gate()
+        self.fgate = self.init_gate()
+        self.ggate = self.init_gate()
+        self.ogate = self.init_gate()
+        self.h_prev = tf.zeros([128,self.num_units])
+        self.c_prev = tf.zeros([128,self.num_units])
+
+    def init_gate(self):
+        initializer = tf.orthogonal_initializer()
+        Wx = initializer([self.input_size,self.num_units])
+        Wh = initializer([self.num_units,self.num_units])
+        b = tf.zeros([self.num_units])
+
+        return Wx,Wh,b
+
+    def callgate(self,X,H,gate,fn):
+        wx,wh,b = gate
+        return fn(tf.matmul(X,wx) + tf.matmul(H,wh) + b)
+
+    def step(self,X):
+        i = self.callgate(X, self.h_prev, self.igate, tf.sigmoid)
+        f = self.callgate(X, self.h_prev, self.fgate, tf.sigmoid)
+        o = self.callgate(X, self.h_prev, self.ogate, tf.sigmoid)
+        g = self.callgate(X, self.h_prev, self.ggate, tf.tanh)
+
+        c = self.c_prev * f + g * i
+        h = tf.tanh(c) * o
+
+        self.h_prev = h
+        self.c_prev = c
+
+        return h
+
+def RNN(cell, X):
+    #h = tf.scan(fn = cell.step, elems = X)
+    for x in X:
+        h = cell.step(x)
+
+    return h
+
+def procdata(X, num_steps, input_size):
+    X = tf.transpose(X,[1,0,2])
+    X = tf.reshape(X,[-1, input_size])
+    X = tf.split(X, num_steps)
+    return X
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+def main():
+
+    mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+    learning_rate = 0.01
+    max_samples = 40000
+    batch_size = 128
+    display_step = 10
+
+    n_input = 28
+    n_steps = 28
+    n_hidden = 256
+    n_classes = 10
+
+    x = tf.placeholder(tf.float32, [None, n_steps, n_input])
+    y = tf.placeholder(tf.float32, [None, n_classes])
+
+    weights = tf.Variable(tf.random_normal([n_hidden, n_classes]))
+    biases = tf.Variable(tf.random_normal([n_classes]))
+
+    # begin lstm
+    x1 = procdata(x,num_steps=n_steps,input_size=n_input)
+    cell = LSTMCell(input_size=n_input, num_units=n_hidden)
+    outputs = RNN(cell, x1)
+    outputs = tf.reshape(outputs, [-1,n_hidden])
+    yhat = tf.matmul(outputs, weights) + biases
+    
+    # cost function
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=yhat,labels=y))
+
+    # optimize
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # compute accuracy
+    correct_pred = tf.equal(tf.argmax(yhat, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+        step = 1
+        while step * batch_size < max_samples:
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            batch_x = batch_x.reshape((batch_size, n_steps, n_input))
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+            if step % display_step == 0:
+                acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+                loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+                print ("Iter" + str(step * batch_size) + ", Minibatch Loss=" + \
+                    "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                    "{:.5f}".format(acc))
+            
+            step += 1
+        print ("Optimization Finishes!")
+
+        test_len = 128
+        test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
+        test_label = mnist.test.labels[:test_len]
+        print ("Testing accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
+
+
+main()
